a8c27365ed362c16d6165349d97bbda6553f5d00
Delfynn2kx <115751754+Delfynn2kx@users.noreply.github.com>
2023-06-27 18:45:43 -0400

    Delete projet1-pokemonscards-dh directory

--- projet1-pokemonscards-dh/src/ImagePokemon.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ImagePokemon = ({ src, alt, hauteur, largeur }) => {
-  return <img src={src} alt={alt} style={{ height: hauteur, width: largeur }} />;
-};
-
-export default ImagePokemon;
